752256257f366fcc0d24863d40e97ede619d4c73
germankern <germankern890@gmail.com>
2023-04-05 18:08:34 -0300

    fix PR and ready formerge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,9 +6,6 @@ export default function Header() {
   return (
     <div className="header">
       <NavBar />
-      <div className="innerContainer">
-        <h2 className="text">Header</h2>
-      </div>
     </div>
   );
 }
