1a476180fbc3019b03a51895a6372603b6bf0af2
germankern <germankern890@gmail.com>
2023-04-10 17:28:39 -0300

    hero section done

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,27 +1,21 @@
 import { MainImage } from 'gatsby-plugin-image';
 import React from 'react';
 import './index.scss';
-import HomeBanner from '../../assets/images/HomeBanner.png';
-import HomeText1 from '../../assets/images/HomeText1.svg';
-import HomeText2 from '../../assets/images/HomeText2.svg';
-import HomeText3 from '../../assets/images/HomeText3.svg';
+import HomeBanner from '../../assets/images/HomeBanner.svg';
+import HomeText from '../../assets/images/HomeText.svg';
 
 const HeroSection = () => {
   return (
-    <div className="heroSection">
-      <MainImage src={HomeBanner} alt="descripcion" />
-      <div className="textHeroSection">
-        <div className="row">
-          <MainImage src={HomeText1} alt="desc" />
-        </div>
-        <div className="row">
-          <MainImage src={HomeText2} alt="desc" />
-        </div>
-        <div className="row">
-          <MainImage src={HomeText3} alt="desc" />
+    <>
+      <div className="heroSection">
+        <MainImage src={HomeBanner} alt="mod benefics" />
+        <div className="textHeroSection">
+          <div className="row">
+            <MainImage src={HomeText} alt="background giant kraken" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
