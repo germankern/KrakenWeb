b51875de569f3ae46db46e025019332f8cc0658a
germankern <germankern890@gmail.com>
2023-05-05 18:06:25 -0300

    fix banners and contact us mobile done

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -11,45 +11,47 @@ const Form = () => {
   return (
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Name*"
-          onChange={(e) => handleChangeFor('name')(e.target.value)}
-          value={formData.name ?? ''}
-          required
-          pattern="[a-zA-Z\s-]+"
-        />
-        <input
-          type="number"
-          placeholder="Your phone number*"
-          onChange={(e) => handleChangeFor('phoneNumber')(e.target.value)}
-          value={formData.phoneNumber ?? ''}
-          required
-          pattern="[\d\- ]{7,15}"
-        />
-        <input
-          type="text"
-          placeholder="Subject*"
-          onChange={(e) => handleChangeFor('subject')(e.target.value)}
-          value={formData.subject ?? ''}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Your mail*"
-          onChange={(e) => handleChangeFor('email')(e.target.value)}
-          value={formData.email ?? ''}
-          required
-          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}"
-        />
-        <input
-          type="text"
-          placeholder="Your message goes here"
-          className="last-child"
-          onChange={(e) => handleChangeFor('message')(e.target.value)}
-          value={formData.message ?? ''}
-          required
-        />
+        <div className="layaout-mobile">
+          <input
+            type="text"
+            placeholder="Name*"
+            onChange={(e) => handleChangeFor('name')(e.target.value)}
+            value={formData.name ?? ''}
+            required
+            pattern="[a-zA-Z\s-]+"
+          />
+          <input
+            type="number"
+            placeholder="Your phone number*"
+            onChange={(e) => handleChangeFor('phoneNumber')(e.target.value)}
+            value={formData.phoneNumber ?? ''}
+            required
+            pattern="[\d\- ]{7,15}"
+          />
+          <input
+            type="text"
+            placeholder="Subject*"
+            onChange={(e) => handleChangeFor('subject')(e.target.value)}
+            value={formData.subject ?? ''}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Your mail*"
+            onChange={(e) => handleChangeFor('email')(e.target.value)}
+            value={formData.email ?? ''}
+            required
+            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}"
+          />
+          <input
+            type="text"
+            placeholder="Your message goes here"
+            className="last-child"
+            onChange={(e) => handleChangeFor('message')(e.target.value)}
+            value={formData.message ?? ''}
+            required
+          />
+        </div>
         <button className="btn-form" type="submit">
           Get in ToUch
         </button>
